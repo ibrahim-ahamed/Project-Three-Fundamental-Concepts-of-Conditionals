8221c9413b99d628be7dd7982e06651842f1ac19
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-28 11:31:30 +0600

    Variable js

---
+++ variable.js
@@ -0,0 +1,32 @@
+/**
+ * VARIABLE ==> container
+ */
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = 'History';
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no qouet
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 89;
+/**
+ * OPARATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNan
+ * 4. null
+ * 5. number
+ */
