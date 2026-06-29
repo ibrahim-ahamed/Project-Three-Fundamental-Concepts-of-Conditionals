ccad535d4529ce0547c31a2d02dfcf5f8b454765
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-29 08:46:23 +0600

    nested js

---
+++ nested.js
@@ -0,0 +1,15 @@
+const money = 0;
+if (money > 300){
+    console.log("I will buy a laptop");
+}
+else{
+    if (money > 100){
+        console.log("I will buy a mobile");
+    }
+    else if (money > 0){
+        console.log("I will buy a book");
+    }
+    else{
+        console.log("I will not buy anything");
+    }
+}
