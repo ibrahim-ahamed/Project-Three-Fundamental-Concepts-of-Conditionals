489d0dc04877aedf7383e09c739703ba522af546
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-29 07:55:28 +0600

    if js

---
+++ if.js
@@ -0,0 +1,27 @@
+// let result = 90;
+
+// if (result >= 80){
+//     console.log("You are golden student");
+// } else if (result >= 70){
+//     console.log(" Your result is A+");
+// } else if (result >= 50){
+//     console.log(" Your result is A");
+// } else if (result >= 33){
+//     console.log(" Your result is A-");
+// } else {
+//     console.log("You failed the test!");
+// }
+
+let result = 90;
+
+if (result >= 80){
+    console.log("You are golden student");
+} else if (result >= 70){
+    console.log(" Your result is A+");
+} else if (result >= 50){
+    console.log(" Your result is A");
+} else if (result >= 33){
+    console.log(" Your result is A-");
+} else {
+    console.log("You failed the test!");
+}
